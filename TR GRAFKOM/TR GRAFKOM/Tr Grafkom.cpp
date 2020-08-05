ae79b3e80fbdd3e4bc7e617593d0cca5f6c80527
Vicky AS <65887817+vick1208@users.noreply.github.com>
2020-08-05 21:37:56 +0700

    Update Tr Grafkom.cpp

--- TR GRAFKOM/TR GRAFKOM/Tr Grafkom.cpp
+++ TR GRAFKOM/TR GRAFKOM/Tr Grafkom.cpp
@@ -1,150 +1,206 @@
-#include <GL/freeglut.h>
 #include <iostream>
-#include <math.h>
-#include <Windows.h>
+#include <vector>
+#include <GL/freeglut.h>
 
-void init(void);
-void disp(void);
-void kb(unsigned char, int, int);
-void kb2(int, int, int);
-void mouse(int button, int state, int x, int y);
-void size(int, int);
-void germou(int x, int y);
+#include "conf.h"
+#include "vector.h"
+#include "nfg.h"
+#include "tga.h"
 
-float xp = 0.0f;
-float yp = 0.0f;
-float xd = 0.0f;
-float yd = 0.0f;
-bool mousedo = false;
-int is_depth;
+using namespace std;
 
-void init() {
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glMatrixMode(GL_PROJECTION);
-	glEnable(GL_DEPTH_TEST);
-	is_depth = 1;
-	glMatrixMode(GL_MODELVIEW);
-	glPointSize(8.0f);
-	glLineWidth(5.0f);
+bool anim = false;
+float xrot = 0.0f;
+float yrot = 0.0f;
+
+float scale = 1.0f;
+float xmov = 0.0f;
+float ymov = 0.0f;
+float zmov = 0.0f;
+
+void LoadTextures() {
+	Tga file = Tga("Woman1.tga");
+
+	glEnable(GL_TEXTURE_2D);
+
+	GLuint texture = 0;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, file.HasAlphaChannel() ? GL_RGBA : GL_RGB, file.GetWidth(), file.GetWidth(), 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, file.GetPixels().data());
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 }
-void disp() {
-	if (is_depth)
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	else
-		glClear(GL_COLOR_BUFFER_BIT);
-	if (mousedo) {
-		glLoadIdentity();
-		glRotatef(xp, 1.0f, 0.0f, 0.0);
-		glRotatef(yp, 0.0f, 1.0f, 0.0f);
-	}
-	
-	
+
+void transform() {
+	glRotatef(xrot, 1.0f, 0.0f, 0.0f); // Rotating horizontal
+	glRotatef(yrot, 0.0f, 1.0f, 0.0f); // Rotating vertical
+	glScalef(scale, scale, scale); // Perskalaan
+	glTranslatef(xmov, ymov, zmov); // Translasi
 }
-void kb(unsigned char k, int x, int y) {
-	switch (k)
-	{
-	case '7':
-		glTranslatef(0.0f, 3.0, 0.0);
-		break;
-	case '9':
-		glTranslatef(0.0f, -3.0f, 0.0f);
-		break;
-	case '2':
-		glRotatef(2.0, 1.0, 0.0, 0.0);
-		break;
-	case '8':
-		glRotatef(-2.0, 1.0, 0.0, 0.0);
-		break;
-	case '6':
-		glRotatef(2.0, 0.0, 1.0, 0.0);
-		break;
-	case '4':
-		glRotatef(-2.0, 0.0, 1.0, 0.0);
-		break;
-	case '1':
-		glRotatef(2.0, 0.0, 0.0, 1.0);
-		break;
-	case '3':
-		glRotatef(-2.0, 0.0, 0.0, 1.0);
-		break;
-	case 27:
-		exit(0);
-		break;
-	}
-	disp();
-}
-void kb2(int k2, int x2, int y2) {
-	if (k2 == GLUT_KEY_LEFT)
-	{
-		glTranslatef(-3.0, 0.0, 0.0);
-	}
-	else if (k2 == GLUT_KEY_UP)
-	{
-		glTranslatef(0.0, 0.0, 3.0);
+
+void display() {
+
+	Nfg model;
+	model.process();
+
+	int NTriangle = model.totalNrIndices / 3;
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glPushMatrix();
+
+	LoadTextures();
+
+	gluLookAt(0.0, 2.0, 8.0, 0.0, 1.5, 5.0, 0, 1, 0);
+
+	transform();
+
+
+	for (int i = 0; i < NTriangle; i++) {
+		
+
+		glBegin(GL_POLYGON);
+		glTexCoord2f(model.myVertex[model.myPattern[i].pos.x].uv.x, model.myVertex[model.myPattern[i].pos.x].uv.y);
+		glVertex3f(model.myVertex[model.myPattern[i].pos.x].pos.x, model.myVertex[model.myPattern[i].pos.x].pos.y, model.myVertex[model.myPattern[i].pos.x].pos.z);
+		glTexCoord2f(model.myVertex[model.myPattern[i].pos.y].uv.x, model.myVertex[model.myPattern[i].pos.y].uv.y);
+		glVertex3f(model.myVertex[model.myPattern[i].pos.y].pos.x, model.myVertex[model.myPattern[i].pos.y].pos.y, model.myVertex[model.myPattern[i].pos.y].pos.z);
+		glTexCoord2f(model.myVertex[model.myPattern[i].pos.z].uv.x, model.myVertex[model.myPattern[i].pos.z].uv.y);
+		glVertex3f(model.myVertex[model.myPattern[i].pos.z].pos.x, model.myVertex[model.myPattern[i].pos.z].pos.y, model.myVertex[model.myPattern[i].pos.z].pos.z);
+		glEnd();
+
+		
+
 	}
 
-	else if (k2 == GLUT_KEY_RIGHT)
-	{
-		glTranslatef(3.0, 0.0, 0.0);
-	}
-	if (k2 == GLUT_KEY_DOWN)
-	{
-		glTranslatef(0.0, 0.0, -3.0);
-	}
-	disp();
+	glPopMatrix();
+	glFlush();
+	glutSwapBuffers();
 }
-void idle() {
-	if (!mousedo) {
-		xp += 0.3f;
-		yp += 0.4f;
-	}
-	glutPostRedisplay();
-}
-void mouse(int button, int state, int x, int y)
+
+void reshape(int w, int h)
 {
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-	{
-
-
-		mousedo = true;
-		xd = x - yp;
-		yd = xp - y;
-		disp();
-	}
-	else
-		mousedo = false;
-	disp();
-}
-void germou(int x, int y) {
-	if (mousedo) {
-		yp = x - xd;
-		xp = y + yd;
-		glutPostRedisplay();
-	}
-}
-void size(int w, int h) {
-	if (h == 0)
-		h = 1;
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(50.0, w / h, 5.0, 500.0);
-	glTranslatef(0.0, -5.0, -150.0);
+
+	if (w > h) {
+		glViewport(0, 0, w, w);
+		glTranslatef(0.0, -0.5, 0.0);
+		glRotatef(180.0, 0.0, 1.0, 0.0);
+		glScalef(-0.5, 0.5, -0.5);
+	}
+	else {
+		glViewport(0, 0, w, h);
+	}
+
+	gluPerspective(20.0, w / h, 0.1f, 100.0f);
+
+	//glTranslatef(0, 0, 0);
+
 	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
-int main(int *argc, char** argv) {
-	glutInit(argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE || GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(0, 0);
+
+void myinit() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glClearDepth(1.0f);
+}
+
+void keyboardControl(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'a':
+		yrot += 5.0f;
+		break;
+	case 'd':
+		yrot -= 5.0f;
+		break;
+	case 'w':
+		xrot += 5.0f;
+		break;
+	case 's':
+		xrot -= 5.0f;
+		break;
+	case 'q':
+		if (anim) {
+			anim = false;
+			printf("Animated!");
+		}
+		else {
+			printf("Animated!");
+			anim = true;
+		}
+		break;
+	case '1':
+		scale += 0.2f;
+		break;
+	case '2':
+		scale -= 0.2f;
+		break;
+	case 27:
+		exit(1);
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+void keyboardSpecialControl(int key, int x, int y) {
+
+	switch (key) {
+	case GLUT_KEY_LEFT:
+		xmov -= 0.025f;
+		break;
+	case GLUT_KEY_UP:
+		ymov += 0.025f;
+		break;
+	case GLUT_KEY_RIGHT:
+		xmov += 0.025f;
+		break;
+	case GLUT_KEY_DOWN:
+		ymov -= 0.025f;
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+void animasi(int) {
+	glutTimerFunc(1000 / 30, animasi, 0);
+
+	if (anim)
+		yrot -= 1.0f;
+	else
+		yrot = yrot;
+
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
+
+	Config conf;
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(conf.winWidth, conf.winHeight);
+	glutInitWindowPosition(conf.winPosX, conf.winPosY);
 	glutCreateWindow("TR GRAFKOM");
-	glutDisplayFunc(disp);
 
-	glutKeyboardFunc(kb);
-	glutSpecialFunc(kb2);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboardControl);
+	glutSpecialFunc(keyboardSpecialControl);
+	glutTimerFunc(0, animasi, 0);
 
-	glutReshapeFunc(size);
-	glutMouseFunc(mouse);
-	glutMotionFunc(germou);
+	cout << " << Keterangan" << endl << endl;
+	cout << " >> W, A, S, D untuk rotasi" << endl;
+	cout << " >> 1, 2 untuk scaling" << endl;
+	cout << " >> Panah untuk move camera" << endl;
+
+	myinit();
 	glutMainLoop();
+
 	return 0;
+
 }
